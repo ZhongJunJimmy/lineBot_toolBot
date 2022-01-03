524e71c88d87e7e81e050b33e5bc1dbb5611d81f
Jimmy Wu <zhong.jun.jimmy@gmail.com>
2022-01-03 09:28:33 +0800

    [TASK] Add package config and function updating

--- index.js
+++ index.js
@@ -1,35 +1,77 @@
-// 引用linebot SDK
 var linebot = require('linebot');
+var moment = require('moment');
 
-// 用於辨識Line Channel的資訊
+// Line Channel info
 var bot = linebot({
   channelId: '1656768637',
   channelSecret: 'a9d199f4de51b79863570da214b058f4',
   channelAccessToken: 'AnnoYMlRarvafN6Eg65Ai1c3+FZzWAXXHUC+RRBHPCKmNg5kEQA4OoOKU1rpovnwbtY8RShDht22BG9rNnJnPOiLZy8tIFOoiHe3RYBRP764hVeT2/W/re4xr5ojlWirmUpsbC2FnA3xtde5TfOCyAdB04t89/1O/w1cDnyilFU='
 });
 
-// 當有人傳送訊息給Bot時
+var debugMode = 0;
+if (process.argv.length > 2){
+	if(process.argv.indexOf("debug") !== -1)
+		debugMode = 1;
+}
+
+
+// message event trigger
 bot.on('message', function (event) {
-  // event.message.text是使用者傳給bot的訊息
-  console.log(event.message.text);
-  var items = event.message.text.split("\n");
-  console.log(items);
-  console.log("length: "+items.length);
-  console.log("----------------");
-  // 使用event.reply(要回傳的訊息)方法可將訊息回傳給使用者
-  event.reply(event.message.text).then(function (data) {
-    // 當訊息成功回傳後的處理
-  }).catch(function (error) {
-    // 當訊息回傳失敗後的處理
-  });
+	event.source.profile().then(function (profile) {
+		var userName = profile.displayName;
+		logMessage("DEBUG", `${userName}說${event.message.text.replace(/\r\n|\n/g,"\\n")}`);
+		var items = event.message.text.split("\n");
+		if(items.indexOf("隨機選擇") !== -1){
+			logMessage("INFO", "偵測\"隨機選擇\"事件,選項包含: "+items);
+			items = items.filter(item => item !== "隨機選擇");
+			if(items.filter(item => item.length > 10).length > 0){
+				logMessage("ERROR", `選項大於10個字元`);
+				var reqMessage = `Hi, ${userName} \n任意選項請不要超過10個字元！`;
+			}else{
+				var reqMessage = `Hi, ${userName} \n你的隨機選擇結果為${ramdonChoose(items)}`;
+			}
+			
+			event.reply(reqMessage).then(function (data) {
+				logMessage("INFO", `已傳送: \"${reqMessage.replace(/\r\n|\n/g,"\\n")}\",至客戶端`);
+			}).catch(function (error) {
+				logMessage("ERROR", error);
+			});
+			
+			
+				
+		}
+	});
+  
 });
 
-// Bot所監聽的webhook路徑與port
 bot.listen('/linewebhook', 3000, function () {
-    console.log('[BOT已準備就緒]');
-    console.log("----------------");
+	debugMode?console.log('[linebot is ready!] (Debug)'):console.log('[linebot is ready!]');
+    
 });
 
-const rendomChoose=(items)=>({
+function ramdonChoose(items){
+	return items[getRandomInt(items.length)];
+}
 
-});
+function getRandomInt(max) {
+	return Math.floor(Math.random() * max);
+}
+
+function logMessage(logType, logMessage){
+	var Timestamp = Date.now();
+	switch(logType){
+		case "ERROR":
+			console.log("\x1b[31m",`[${moment(Timestamp).format('YYYY-MM-DDTHH:mm:ss.SSS')}] ERROR: ${logMessage}`);
+			break;
+		case "INFO":
+			console.log("\x1b[32m",	`[${moment(Timestamp).format('YYYY-MM-DDTHH:mm:ss.SSS')}] INFO: ${logMessage}`);
+			break;
+		case "DEBUG":
+			if(debugMode === 1) console.log("\x1b[34m",`[${moment(Timestamp).format('YYYY-MM-DDTHH:mm:ss.SSS')}] DEBUG: ${logMessage}`);
+				
+			break;
+		default:
+			console.log("\x1b[31m","[`${timeInMs}`] Error: The log type does not existed");
+	}
+		
+}
