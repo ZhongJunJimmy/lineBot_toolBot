95939b6a538e7c9314b248870dbea3f434fea64d
Jimmy Wu <zhong.jun.jimmy@gmail.com>
2022-01-05 15:13:56 +0800

    [FIX] fix download file name different issue

--- index.js
+++ index.js
@@ -174,7 +174,7 @@ function getWeatherInfo(event, userName, location, area){
 				fileList.filter((file)=>file.indexOf(twFileName) !== -1);
 				logMessage.log(debugMode, "DEBUG", `tw file name: ${fileList[0]}`);
 				let twData = JSON.parse(fs.readFileSync(`./data/${fileList[0]}`));
-				twData.cwbopendataldataset[0].locations[0].location.map((element)=>{
+				twData.cwbopendataldataset.locations[0].location.map((element)=>{
 					locDescription.push({
 						locationName: element.locationName[0],
 						weatherDescription: element.weatherElement[10].time[0].elementValue[0].value[0]
